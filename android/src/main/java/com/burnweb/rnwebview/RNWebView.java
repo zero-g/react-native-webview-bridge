ee9697dd775a73ab61758f17c81c074789964e59
lishaoyuan <lishaoyuan@meituan.com>
2016-03-16 11:28:39 +0800

    ADD: extends android bridge

---
+++ android/src/main/java/com/burnweb/rnwebview/RNWebView.java
@@ -0,0 +1,118 @@
+package com.burnweb.rnwebview;
+
+import android.os.SystemClock;
+import android.util.Log;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.uimanager.UIManagerModule;
+import com.facebook.react.uimanager.events.EventDispatcher;
+
+/*package*/ class RNWebView extends WebView {
+
+    protected class GeoWebChromeClient extends WebChromeClient {
+        @Override
+        public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+            callback.invoke(origin, true, false);
+        }
+    }
+
+    protected class EventWebClient extends WebViewClient {
+
+        private String injectedJavaScript = null;
+
+        public void setInjectedJavaScript(String injectedJavaScript) {
+            this.injectedJavaScript = injectedJavaScript;
+        }
+
+        public String getInjectedJavaScript() {
+            return this.injectedJavaScript;
+        }
+
+        public void onPageFinished(WebView view, String url) {
+            mEventDispatcher.dispatchEvent(
+                    new NavigationStateChangeEvent(getId(), SystemClock.uptimeMillis(), view.getTitle(), false, url, view.canGoBack(), view.canGoForward()));
+
+            if(getInjectedJavaScript() != null) {
+                view.loadUrl("javascript:(function() { " + getInjectedJavaScript() + "})()");
+            }
+        }
+
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            mEventDispatcher.dispatchEvent(
+                    new NavigationStateChangeEvent(getId(), SystemClock.uptimeMillis(), view.getTitle(), true, url, view.canGoBack(), view.canGoForward()));
+        }
+    }
+
+    private final EventDispatcher mEventDispatcher;
+    private final EventWebClient mWebViewClient;
+    private String charset = "UTF-8";
+    private String baseUrl = "file:///";
+
+    public RNWebView(ReactContext reactContext) {
+        super(reactContext);
+
+        mEventDispatcher = reactContext.getNativeModule(UIManagerModule.class).getEventDispatcher();
+        mWebViewClient = new EventWebClient();
+
+        this.getSettings().setJavaScriptEnabled(true);
+        this.getSettings().setBuiltInZoomControls(false);
+        this.getSettings().setDomStorageEnabled(true);
+        this.getSettings().setGeolocationEnabled(false);
+        this.getSettings().setPluginState(WebSettings.PluginState.ON);
+        this.getSettings().setAllowFileAccess(true);
+        this.getSettings().setAllowFileAccessFromFileURLs(true);
+        this.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        this.getSettings().setLoadsImagesAutomatically(true);
+        this.getSettings().setBlockNetworkImage(false);
+        this.getSettings().setBlockNetworkLoads(false);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            this.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+
+        this.setWebViewClient(mWebViewClient);
+        this.setWebChromeClient(new WebChromeClient());
+    }
+
+    public void setCharset(String charset) {
+        this.charset = charset;
+    }
+
+    public String getCharset() {
+        return this.charset;
+    }
+
+    public void setInjectedJavaScript(String injectedJavaScript) {
+        mWebViewClient.setInjectedJavaScript(injectedJavaScript);
+    }
+
+    public String getInjectedJavaScript() {
+        return mWebViewClient.getInjectedJavaScript();
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String getBaseUrl() {
+        return this.baseUrl;
+    }
+
+    public GeoWebChromeClient getGeoClient() {
+        return new GeoWebChromeClient();
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        this.loadDataWithBaseURL(this.getBaseUrl(), "<html></html>", "text/html", this.getCharset(), null);
+    }
+}
---
+++ android/src/main/java/com/burnweb/rnwebview/RNWebViewManager.java
@@ -0,0 +1,151 @@
+package com.burnweb.rnwebview;
+
+import javax.annotation.Nullable;
+
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.CookieManager;
+
+import com.facebook.react.bridge.JSApplicationIllegalArgumentException;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.common.MapBuilder;
+import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+import com.facebook.react.common.annotations.VisibleForTesting;
+
+import java.util.Map;
+
+public class RNWebViewManager extends ViewGroupManager<RNWebView> {
+
+    public static final int GO_BACK = 1;
+    public static final int GO_FORWARD = 2;
+    public static final int RELOAD = 3;
+
+    @VisibleForTesting
+    public static final String REACT_CLASS = "RNWebViewAndroid";
+
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
+    @Override
+    public RNWebView createViewInstance(ThemedReactContext context) {
+        RNWebView rnwv = new RNWebView(context);
+
+        // default size
+        rnwv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                                    ViewGroup.LayoutParams.MATCH_PARENT));
+
+        CookieManager.getInstance().setAcceptCookie(true); // add default cookie support
+        CookieManager.getInstance().setAcceptFileSchemeCookies(true); // add default cookie support
+
+        return rnwv;
+    }
+
+    @ReactProp(name = "disableCookies", defaultBoolean = false)
+    public void setDisableCookies(RNWebView view, boolean disableCookies) {
+        if(disableCookies) {
+            CookieManager.getInstance().setAcceptCookie(false);
+            CookieManager.getInstance().setAcceptFileSchemeCookies(false);
+        } else {
+            CookieManager.getInstance().setAcceptCookie(true);
+            CookieManager.getInstance().setAcceptFileSchemeCookies(true);
+        }
+    }
+
+    @ReactProp(name = "disablePlugins", defaultBoolean = false)
+    public void setDisablePlugins(RNWebView view, boolean disablePlugins) {
+        if(disablePlugins) {
+            view.getSettings().setPluginState(WebSettings.PluginState.OFF);
+        } else {
+            view.getSettings().setPluginState(WebSettings.PluginState.ON);
+        }
+    }
+
+    @ReactProp(name = "builtInZoomControls", defaultBoolean = false)
+    public void setBuiltInZoomControls(RNWebView view, boolean builtInZoomControls) {
+        view.getSettings().setBuiltInZoomControls(builtInZoomControls);
+    }
+
+    @ReactProp(name = "geolocationEnabled", defaultBoolean = false)
+    public void setGeolocationEnabled(RNWebView view, boolean geolocationEnabled) {
+        view.getSettings().setGeolocationEnabled(geolocationEnabled);
+
+        if(geolocationEnabled) {
+            view.setWebChromeClient(view.getGeoClient());
+        }
+        else {
+            view.setWebChromeClient(new WebChromeClient());
+        }
+    }
+
+    @ReactProp(name = "javaScriptEnabled", defaultBoolean = true)
+    public void setJavaScriptEnabled(RNWebView view, boolean javaScriptEnabled) {
+        view.getSettings().setJavaScriptEnabled(javaScriptEnabled);
+    }
+
+    @ReactProp(name = "userAgent")
+    public void setUserAgent(RNWebView view, @Nullable String userAgent) {
+        if(userAgent != null) view.getSettings().setUserAgentString(userAgent);
+    }
+
+    @ReactProp(name = "url")
+    public void setUrl(RNWebView view, @Nullable String url) {
+        view.loadUrl(url);
+    }
+
+    @ReactProp(name = "baseUrl")
+    public void setBaseUrl(RNWebView view, @Nullable String baseUrl) {
+        view.setBaseUrl(baseUrl);
+    }
+
+    @ReactProp(name = "htmlCharset")
+    public void setHtmlCharset(RNWebView view, @Nullable String htmlCharset) {
+        if(htmlCharset != null) view.setCharset(htmlCharset);
+    }
+
+    @ReactProp(name = "html")
+    public void setHtml(RNWebView view, @Nullable String html) {
+        view.loadDataWithBaseURL(view.getBaseUrl(), html, "text/html", view.getCharset(), null);
+    }
+
+    @ReactProp(name = "injectedJavaScript")
+    public void setInjectedJavaScript(RNWebView view, @Nullable String injectedJavaScript) {
+        view.setInjectedJavaScript(injectedJavaScript);
+    }
+
+    @Override
+    public @Nullable Map<String, Integer> getCommandsMap() {
+        return MapBuilder.of(
+            "goBack", GO_BACK,
+            "goForward", GO_FORWARD,
+            "reload", RELOAD
+        );
+    }
+
+    @Override
+    public void receiveCommand(RNWebView view, int commandId, @Nullable ReadableArray args) {
+        switch (commandId) {
+            case GO_BACK:
+                view.goBack();
+                break;
+            case GO_FORWARD:
+                view.goForward();
+                break;
+            case RELOAD:
+                view.reload();
+                break;
+        }
+    }
+
+    @Override
+    public Map getExportedCustomDirectEventTypeConstants() {
+        return MapBuilder.of(
+                NavigationStateChangeEvent.EVENT_NAME, MapBuilder.of("registrationName", "onNavigationStateChange")
+        );
+    }
+
+}
